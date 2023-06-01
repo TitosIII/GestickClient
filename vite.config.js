df576d1b23602b3f2cd7482adebe572c97122b94
Your Name <you@example.com>
2023-06-01 00:17:49 -0600

    start script modified

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import {esbuildCommonjs} from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps:{
+    esbuildOptions:{
+      plugins:[
+        esbuildCommonjs(['quagga', 'react-scripts', 'react-dom'])
+      ]
+    }
+  },
   server: {
     port: process.env.PORT || 5173,
   },
